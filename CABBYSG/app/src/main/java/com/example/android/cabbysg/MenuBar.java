3b60594d4e12b10df5e8e161e206c57d9e073eba
Calartian <calartiankoh@gmail.com>
2018-05-10 15:20:39 +0800

    upload base structure of app

---
+++ CABBYSG/app/src/main/java/com/example/android/cabbysg/MenuBar.java
@@ -0,0 +1,121 @@
+package com.example.android.cabbysg;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MenuBar extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setLogo(R.drawable.app_logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.app_logo);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        setTitle("Home");
+        nav_home fragment = new nav_home();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame, fragment,"Home");
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+        setTitle("Home");
+        nav_home fragment = new nav_home();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame, fragment,"Home");
+        fragmentTransaction.commit();
+
+        } else if (id == R.id.nav_profile) {
+            setTitle("Profile");
+            nav_profile fragment = new nav_profile();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, fragment,"Profile");
+            fragmentTransaction.commit();
+
+        } else if (id == R.id.nav_payment) {
+            setTitle("Payment");
+            nav_payment fragment = new nav_payment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, fragment,"Payment");
+            fragmentTransaction.commit();
+
+        } else if (id == R.id.nav_schedule) {
+            setTitle("Schedule");
+            nav_schedule fragment = new nav_schedule();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, fragment,"Schedule");
+            fragmentTransaction.commit();
+
+        } else if (id == R.id.nav_history) {
+            setTitle("History");
+            nav_history fragment = new nav_history();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, fragment,"History");
+            fragmentTransaction.commit();
+
+        } else if (id == R.id.nav_lostandfound) {
+            setTitle("Lost & Found");
+            nav_lostandfound fragment = new nav_lostandfound();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, fragment,"LostandFound");
+            fragmentTransaction.commit();
+
+        } else if (id == R.id.nav_help) {
+            setTitle("Help");
+            nav_help fragment = new nav_help();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, fragment,"Help");
+            fragmentTransaction.commit();
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ CABBYSG/app/src/main/java/com/example/android/cabbysg/spinnerdropdown.java
@@ -0,0 +1,14 @@
+package com.example.android.cabbysg;
+
+/*    String text;
+    Integer imageId;
+    public spinnerdropdown(String text, Integer imageId) {
+        this.text=text;
+        this.imageId=imageId;
+    }
+    public String getText() {
+        return text;
+    }
+    public Integer
+}
+*/
