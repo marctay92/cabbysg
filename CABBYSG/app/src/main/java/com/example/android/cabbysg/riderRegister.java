f91e8882c5b88f4f442617818df8b54112717d50
marctay92 <39039532+marctay92@users.noreply.github.com>
2018-06-01 15:36:17 +0800

    added check for setting value to check if successfully added

--- CABBYSG/app/src/main/java/com/example/android/cabbysg/riderRegister.java
+++ CABBYSG/app/src/main/java/com/example/android/cabbysg/riderRegister.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,8 +22,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
 import java.util.HashMap;
@@ -148,7 +153,16 @@ public class riderRegister extends AppCompatActivity {
                                 newPost.put("dob",str_dob);
                                 newPost.put("mobileNum",str_mobileNb);
 
-                                current_user_db.setValue(newPost);
+                                current_user_db.setValue(newPost).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()){
+                                            Toast.makeText(riderRegister.this,"Registration is successful",Toast.LENGTH_SHORT);
+                                        }else{
+                                            Toast.makeText(riderRegister.this,"Registration is unsuccessful",Toast.LENGTH_SHORT);
+                                        }
+                                    }
+                                });
                             }
                         }
                     });
