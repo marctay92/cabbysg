d196538bda62e6bc780df0cd7f163cfd39ed36c3
marctay92 <39039532+marctay92@users.noreply.github.com>
2018-06-04 15:52:24 +0800

    profile able to handle recent login exception and remove DOB variable for registration for riders

--- CABBYSG/app/src/main/java/com/example/android/cabbysg/editProfile.java
+++ CABBYSG/app/src/main/java/com/example/android/cabbysg/editProfile.java
@@ -1,12 +1,14 @@
 package com.example.android.cabbysg;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -19,8 +21,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -32,12 +38,21 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.HashMap;
 import java.util.Map;
 
+import static android.app.ProgressDialog.STYLE_SPINNER;
 import static android.content.ContentValues.TAG;
 
 public class editProfile extends Fragment{
 
     EditText firstNameEditText, lastNameEditText, emailEditText, mobileEditText;
+    String firstNameStr, lastNameStr, mobileStr, emailStr;
+    boolean validEditFirst = false;
+    boolean validEditLast=false;
+    boolean validEditMobile=false;
+    boolean validEditEmail=false;
     Button deleteAccBtn, saveChangesBtn;
+
+    //Create Progress Dialog
+    ProgressDialog pd;
 
     DatabaseReference current_user_db;
 
@@ -58,6 +73,13 @@ public class editProfile extends Fragment{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View rootView = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+
+        //Init pd
+        pd = new ProgressDialog(getActivity(), STYLE_SPINNER);
+        //Set cancelable to not let users click it away
+        pd.setCancelable(false);
+        //Set message and show
+        pd.setMessage("Please Wait...");
 
        //Edit Text fields
        firstNameEditText = rootView.findViewById(R.id.firstNameEdit);
@@ -142,14 +164,10 @@ public class editProfile extends Fragment{
         saveChangesBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 //Conversion to string
-                final String firstNameStr = firstNameEditText.getText().toString();
-                final String lastNameStr = lastNameEditText.getText().toString();
-                final String mobileStr = mobileEditText.getText().toString();
-                final String emailStr = emailEditText.getText().toString();
-                boolean validEditFirst = false;
-                boolean validEditLast=false;
-                boolean validEditMobile=false;
-                boolean validEditEmail=false;
+                firstNameStr = firstNameEditText.getText().toString();
+                lastNameStr = lastNameEditText.getText().toString();
+                mobileStr = mobileEditText.getText().toString();
+                emailStr = emailEditText.getText().toString();
 
                 //Condition to check
                 if(TextUtils.isEmpty(firstNameStr)) {
@@ -169,39 +187,106 @@ public class editProfile extends Fragment{
                 } else validEditMobile = true;
 
                 if(validEditFirst&&validEditLast&&validEditEmail&&validEditMobile){
-                    if(!user.getEmail().equals(emailStr)) {
-                        user.updateEmail(emailStr).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (!task.isSuccessful()) {
-                                    Toast.makeText(getActivity(), "User email address is taken or incorrect.",Toast.LENGTH_SHORT).show();
-                                }
+                    pd.show();
+                    user.updateEmail(emailStr).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                String user_id = mAuth.getCurrentUser().getUid();
+                                DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Rider").child(user_id);
+
+                                Map newPost = new HashMap();
+                                //newPost.put("email",str_email);
+                                newPost.put("firstName", firstNameStr);
+                                newPost.put("lastName", lastNameStr);
+                                newPost.put("mobileNum", mobileStr);
+                                newPost.put("email",emailStr);
+
+                                current_user_db.setValue(newPost);
+
+                                Fragment newFragment=new nav_profile();
+                                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                                transaction.replace(R.id.editProfileFragment,newFragment);
+                                transaction.addToBackStack(null);
+                                transaction.commit();
+                                pd.dismiss();
+
                             }
-                        });
-                    }else{
-                        newPost.put("email",emailStr);
-                    }
-                    String user_id = mAuth.getCurrentUser().getUid();
-                    DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Rider").child(user_id);
-
-                    Map newPost = new HashMap();
-                    //newPost.put("email",str_email);
-                    newPost.put("firstName", firstNameStr);
-                    newPost.put("lastName", lastNameStr);
-                    newPost.put("mobileNum", mobileStr);
-
-                    current_user_db.setValue(newPost);
-
-                    Fragment newFragment=new nav_profile();
-                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                    transaction.replace(R.id.editProfileFragment,newFragment);
-                    transaction.addToBackStack(null);
-                    transaction.commit();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            if (e instanceof FirebaseAuthRecentLoginRequiredException){
+                                showDialog();
+                                pd.dismiss();
+                            }
+                        }
+                    });
                 }
             }
         });
 
        return rootView;
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Re-key in your credentials");
+
+        // Set up the input
+        final String email = user.getEmail();
+        final EditText password = new EditText(getActivity());
+        // Specify the type of input expected
+        password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        password.setHint("Please Enter Your Password");
+        password.setHintTextColor(getResources().getColor(R.color.text_color_primary));
+        builder.setView(password);
+
+        // Set up the buttons
+        builder.setPositiveButton("Re-login", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Put function after clicking OK
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // Get auth credentials from the user for re-authentication. The example below shows
+                // email and password credentials but there are multiple possible providers,
+                // such as GoogleAuthProvider or FacebookAuthProvider.
+                AuthCredential credential = EmailAuthProvider
+                        .getCredential(email, password.getText().toString());
+
+                // Prompt the user to re-provide their sign-in credentials
+                user.reauthenticate(credential)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    System.out.println("User re-authenticated.");
+                                    String user_id = mAuth.getCurrentUser().getUid();
+                                    DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Rider").child(user_id);
+
+                                    Map newPost = new HashMap();
+                                    //newPost.put("email",str_email);
+                                    newPost.put("firstName", firstNameStr);
+                                    newPost.put("lastName", lastNameStr);
+                                    newPost.put("mobileNum", mobileStr);
+                                    newPost.put("email",emailStr);
+
+                                    current_user_db.setValue(newPost);
+
+                                    Fragment newFragment=new nav_profile();
+                                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                                    transaction.replace(R.id.editProfileFragment,newFragment);
+                                    transaction.addToBackStack(null);
+                                    transaction.commit();
+                                }
+                            }
+                        });
+            }
+        });
+
+        builder.show();
     }
 
     /*@Override
--- CABBYSG/app/src/main/java/com/example/android/cabbysg/riderRegister.java
+++ CABBYSG/app/src/main/java/com/example/android/cabbysg/riderRegister.java
@@ -38,7 +38,6 @@ import java.util.regex.Pattern;
 
 public class riderRegister extends AppCompatActivity {
     EditText firstName, lastName, email, mobileNb, password, rePassword;
-    static EditText dob;
     ImageView profilePic;
     Button registerButton;
 
@@ -66,18 +65,11 @@ public class riderRegister extends AppCompatActivity {
 
         firstName = findViewById(R.id.firstName);
         lastName = findViewById(R.id.lastName);
-        dob = findViewById(R.id.dob);
         email = findViewById(R.id.email);
         mobileNb = findViewById(R.id.mobileNb);
         password = findViewById(R.id.password);
         rePassword = findViewById(R.id.rePassword);
         profilePic = findViewById(R.id.profilePic);
-        dob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDatePickerDialog(v);
-            }
-        });
 
         registerButton = findViewById(R.id.registerButton);
 
@@ -86,7 +78,6 @@ public class riderRegister extends AppCompatActivity {
             public void onClick(View v) {
                 final String str_firstName = firstName.getText().toString();
                 final String str_lastName = lastName.getText().toString();
-                final String str_dob = dob.getText().toString();
                 final String str_email = email.getText().toString();
                 final String str_mobileNb = mobileNb.getText().toString();
                 final String str_password = password.getText().toString();
@@ -94,7 +85,6 @@ public class riderRegister extends AppCompatActivity {
 
                 boolean validFirstName = false;
                 boolean validLastName = false;
-                boolean validDob = false;
                 boolean validEmail = false;
                 boolean validNum = false;
                 boolean validPW = false;
@@ -107,10 +97,6 @@ public class riderRegister extends AppCompatActivity {
                 if (TextUtils.isEmpty(str_lastName)) {
                     lastName.setError("Please enter your last name");
                 } else validLastName = true;
-
-                if (TextUtils.isEmpty(str_dob)) {
-                    dob.setError("Please enter your date of birth");
-                } else validDob = true;
 
                 if (TextUtils.isEmpty(str_email)) {
                     email.setError("Please enter your email");
@@ -135,7 +121,7 @@ public class riderRegister extends AppCompatActivity {
                     password.setError("Password does not match");
                 } else validRePW = true;
 
-                if (validFirstName && validLastName && validDob && validEmail && validNum && validPW && validRePW){
+                if (validFirstName && validLastName && validEmail && validNum && validPW && validRePW){
                     mAuth.createUserWithEmailAndPassword(str_email,str_password).addOnCompleteListener(riderRegister.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
@@ -149,7 +135,6 @@ public class riderRegister extends AppCompatActivity {
                                 //newPost.put("email",str_email);
                                 newPost.put("firstName",str_firstName);
                                 newPost.put("lastName",str_lastName);
-                                newPost.put("dob",str_dob);
                                 newPost.put("mobileNum",str_mobileNb);
                                 newPost.put("email",mAuth.getCurrentUser().getEmail());
 
@@ -196,31 +181,5 @@ public class riderRegister extends AppCompatActivity {
         return matcher.matches();
 
     }
-
-    public static class DatePickerFragment extends DialogFragment implements
-            DatePickerDialog.OnDateSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
-        }
-
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
-            dob.setText(year + "-" + (month+1) + "-" + day);
-        }
-    }
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
-    }
-
     
 }
