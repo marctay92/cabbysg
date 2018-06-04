78b57a5498a49b1ff7c66bb5b6ab4ff0ad0459a4
samuelteowj <39039618+samuelteowj@users.noreply.github.com>
2018-06-04 12:21:19 +0800

    create request accepted

---
+++ CABBYSG/app/src/main/java/com/example/android/cabbysg/nav_request_accepted.java
@@ -0,0 +1,23 @@
+package com.example.android.cabbysg;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class nav_request_accepted extends Fragment {
+    public nav_request_accepted() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nav_request_accepted, container, false);
+    }
+
+}
