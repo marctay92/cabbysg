2d55be1d361e525609fcc284661f1cc605f47185
Calartian <calartiankoh@gmail.com>
2018-06-13 16:42:54 +0800

    user manual left images

--- CABBYSG/app/src/main/java/com/example/android/cabbysg/driverManual.java
+++ CABBYSG/app/src/main/java/com/example/android/cabbysg/driverManual.java
@@ -3,22 +3,30 @@ package com.example.android.cabbysg;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class driverManual extends Fragment {
 
+    ViewPager viewPager;
+    ViewPageAdapter adapter;
 
     public driverManual() {
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView= inflater.inflate(R.layout.fragment_driver_manual, container, false);
+
+        viewPager=(ViewPager)rootView.findViewById(R.id.viewPager);
+        adapter = new ViewPageAdapter(getActivity());
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(adapter.getCount()-3);
+
         return rootView;
     }
 
--- CABBYSG/app/src/main/java/com/example/android/cabbysg/userManual.java
+++ CABBYSG/app/src/main/java/com/example/android/cabbysg/userManual.java
@@ -4,16 +4,28 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class userManual extends Fragment {
+    ViewPager viewPager;
+    riderManualAdapter adapter;
+
+    public userManual(){
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView= inflater.inflate(R.layout.fragment_user_manual, container, false);
+        viewPager=(ViewPager)rootView.findViewById(R.id.viewPager);
+        adapter = new riderManualAdapter(getActivity());
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(adapter.getCount()-3);
         return rootView;
     }
 }
